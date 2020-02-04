496dda4eac971924d8435dbcffddd5db166659ef
Alan-Liang <a@keeer.net>
2020-02-04 22:16:04 +0800

    Initial Commit

---
+++ Assets/GenerateOther.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateOther : MonoBehaviour {
+
+	GameObject prefab;
+	float speed = 4.0f;
+	// Use this for initialization
+	void Start () {
+		this.prefab = Instantiate((GameObject) Resources.Load("Other"));
+		float theta = Random.Range(1f, 360f);
+		this.prefab.transform.position = new Vector3(15f*(float)System.Math.Cos(System.Math.PI/360f*theta), 0.5f, 15f*(float)System.Math.Sin(System.Math.PI/360f*theta));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float theta = Random.Range(1f, 360f);
+		Vector3 baseT = new Vector3(1f*(float)System.Math.Cos(System.Math.PI/360f*theta), 0f, 1f*(float)System.Math.Sin(System.Math.PI/360f*theta))*this.speed;
+		this.prefab.transform.Translate(baseT * Time.deltaTime);
+	}
+}
---
+++ Assets/State.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour {
+
+	float startTime = -1f;
+	bool locked = false;
+	public UnityEngine.UI.Text label;
+	// Use this for initialization
+	void Start () {
+		this.label.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.locked) return;
+		float dist = Vector3.Distance(transform.position, new Vector3(0, 0, 0));
+		if (dist < 5f) {
+			if (startTime == -1f) {
+				startTime = Time.time;
+			} else if (Time.time - startTime > 5.0f) {
+				this.label.text = "You Won!";
+				this.label.enabled = true;
+				this.locked = true;
+			}
+		} else if (dist > 50f) {
+			this.label.text = "You Lost...";
+			this.label.enabled = true;
+			this.locked = true;
+		}
+	}
+}
